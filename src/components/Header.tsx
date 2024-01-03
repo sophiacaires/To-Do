adea05700bf6ab394f707682be3934f6b19c15bc
Sophia <soph.caires@gmail.com>
2024-01-03 18:56:43 -0300

    icon and logo update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
 import styles from './Header.module.css'
 
-import toDoLogo from '../assets/to-do-list.svg'
+import toDoLogo from '../assets/to-do-logo.svg'
 
 export function Header(){
     return (
         <header className={styles.header}>
             <img src={toDoLogo} alt="To-do logo" />
-            <p className='cond-bold-upper'>To-Do List</p>
         </header>
     )
 }
